bfd00e468ef484306f03642b34e511b47eb4c761
Patricia Alvarez <168550797+PatriciaAlEs@users.noreply.github.com>
2025-04-05 16:07:48 +0000

    ejercicio semaforo basico

---
+++ src/js/components/Semaforo.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useState } from "react";
+
+export const Semaforo = () => {
+
+    const [color, setColor] = useState("luzRoja")
+
+    const changeColor = (val) => {
+        setColor(val)
+        console.log(val);
+    }
+    
+    return (
+        <>
+            <h2>Aqui iria mi semaforo</h2>
+            <div className="contenedorSemaforo">
+                <div className={`luzRoja" ${color == "luzRoja" ? "luzRoja glow" : "luzRoja"}`} onClick={()=>changeColor("luzRoja")}></div>
+                <div className={`luzAmbar" ${color == "luzAmbar" ? "luzAmbar glow" : "luzAmbar"}`} onClick={()=>changeColor("luzAmbar")}></div>
+                <div className={`luzVerde" ${color == "luzVerde" ? "luzVerde glow" : "luzVerde"}`} onClick={()=>changeColor("luzVerde")}></div>
+            </div>
+        </>
+    )
+}
