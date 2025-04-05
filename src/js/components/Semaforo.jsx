5b0567b3bfe20048ff4340c57a0f0529067ad163
Patricia Alvarez <168550797+PatriciaAlEs@users.noreply.github.com>
2025-04-05 16:10:33 +0000

    ejercicio semaforo basico ok

--- src/js/components/Semaforo.jsx
+++ src/js/components/Semaforo.jsx
@@ -14,9 +14,9 @@ export const Semaforo = () => {
         <>
             <h2>Aqui iria mi semaforo</h2>
             <div className="contenedorSemaforo">
-                <div className={`luzRoja" ${color == "luzRoja" ? "luzRoja glow" : "luzRoja"}`} onClick={()=>changeColor("luzRoja")}></div>
-                <div className={`luzAmbar" ${color == "luzAmbar" ? "luzAmbar glow" : "luzAmbar"}`} onClick={()=>changeColor("luzAmbar")}></div>
-                <div className={`luzVerde" ${color == "luzVerde" ? "luzVerde glow" : "luzVerde"}`} onClick={()=>changeColor("luzVerde")}></div>
+                <div className={`luzRoja ${color == "luzRoja" ? "luzRoja glow" : "luzRoja"}`} onClick={()=>changeColor("luzRoja")}></div>
+                <div className={`luzAmbar ${color == "luzAmbar" ? "luzAmbar glow" : "luzAmbar"}`} onClick={()=>changeColor("luzAmbar")}></div>
+                <div className={`luzVerde ${color == "luzVerde" ? "luzVerde glow" : "luzVerde"}`} onClick={()=>changeColor("luzVerde")}></div>
             </div>
         </>
     )
